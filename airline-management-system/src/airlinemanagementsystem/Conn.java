741fd955563df6fc63851997b472072ab0973af6
refatkhan <refatkhan897@gmail.com>
2024-12-08 21:59:46 +0600

    connect sql

---
+++ airline-management-system/src/airlinemanagementsystem/Conn.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airlinemanagementsystem;
+
+import java.sql.*;
+
+/**
+ *
+ * @author REFAT KHAN
+ */
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    
+    public Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem", "root", "codeforinterview");
+            s = c.createStatement();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
