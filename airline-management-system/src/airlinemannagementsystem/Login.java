22a73ae482e9be38674d60abda526a7ecc58f145
refatkhan <refatkhan897@gmail.com>
2024-12-08 21:45:29 +0600

    add level for username

--- airline-management-system/src/airlinemannagementsystem/Login.java
+++ airline-management-system/src/airlinemannagementsystem/Login.java
@@ -14,10 +14,18 @@ public class Login extends JFrame {
         //frame colour change
         getContentPane().setBackground(Color.yellow);
         setLayout(null);
-        
-        JLabel lblusername = new JLabel ("username ");
+        //username create
+        JLabel lblusername = new JLabel ("Username: ");
         lblusername.setBounds(20, 20, 100, 20);
         add(lblusername);
+        JTextField tfusername = new JTextField();
+        tfusername.setBounds(100, 20, 150, 20);
+        add(tfusername);
+        
+         //password create
+        JLabel lblpassword = new JLabel ("Password: ");
+        lblpassword.setBounds(20, 60, 100, 20);
+        add(lblpassword);
         //first size increase width second height
         setSize(500,350);
         setLocation(600,250);
