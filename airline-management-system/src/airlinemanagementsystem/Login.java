8755d6a3e67899ee461fb496794a593cfa3ebfbc
refatkhan <refatkhan897@gmail.com>
2024-12-08 22:04:42 +0600

    add username,password,forgetpass,submit and colse button

--- airline-management-system/src/airlinemanagementsystem/Login.java
+++ airline-management-system/src/airlinemanagementsystem/Login.java
@@ -2,16 +2,14 @@ package airlinemanagementsystem;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.sql.*;
 
-public class Login extends JFrame implements ActionListener{
+public class Login extends JFrame {
     JButton submit, reset, close;
     JTextField tfusername;
     JPasswordField tfpassword;
     
     public Login() {
-        getContentPane().setBackground(Color.WHITE);
+        getContentPane().setBackground(Color.yellow);
         setLayout(null);
         
         JLabel lblusername = new JLabel("Username");
@@ -32,17 +30,17 @@ public class Login extends JFrame implements ActionListener{
         
         reset = new JButton("Reset");
         reset.setBounds(40, 120, 120, 20);
-        reset.addActionListener(this);
+        
         add(reset);
         
         submit = new JButton("Submit");
         submit.setBounds(190, 120, 120, 20);
-        submit.addActionListener(this);
+       
         add(submit);
         
         close = new JButton("Close");
         close.setBounds(120, 160, 120, 20);
-        close.addActionListener(this);
+       
         add(close);
         
         setSize(400, 250);
@@ -50,37 +48,8 @@ public class Login extends JFrame implements ActionListener{
         setVisible(true);
     }
     
-    public void actionPerformed(ActionEvent ae) {
-        if (ae.getSource() == submit) {
-            String username = tfusername.getText();
-            String password = tfpassword.getText();
-            
-            try {
-                Conn c = new Conn();
-                
-                String query = "select * from login where username = '"+username+"' and password = '"+password+"'";
-                
-                ResultSet rs = c.s.executeQuery(query);
-                
-                if (rs.next()) {
-                    new Home();
-                    setVisible(false);
-                } else {
-                    JOptionPane.showMessageDialog(null, "Invalid Username or Password");
-                    setVisible(false);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else if (ae.getSource() == close) {
-            setVisible(false);
-        } else if (ae.getSource() == reset) {
-            tfusername.setText("");
-            tfpassword.setText("");
-        }
-    }
-    
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         new Login();
     }
+
 }
