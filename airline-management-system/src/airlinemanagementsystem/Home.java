36a50123082b048e4b0d92de2ce798bb5c069f02
refatkhan <refatkhan897@gmail.com>
2024-12-09 21:20:35 +0600

    add text level behind the background image

--- airline-management-system/src/airlinemanagementsystem/Home.java
+++ airline-management-system/src/airlinemanagementsystem/Home.java
@@ -14,6 +14,11 @@ public class Home extends JFrame implements ActionListener {
         JLabel image = new JLabel(imageIcon);
         image.setBounds(0, 0, 1600, 800);
         add(image);
+        JLabel heading = new JLabel("WLCOME TO BANGLADESH");
+        heading.setBounds(500, 20, 1000, 40);
+        heading.setForeground(Color.GREEN);
+        heading.setFont(new Font("Tahome", Font.BOLD, 36));
+        image.add(heading);
 
         // setSize(600, 350);
         // setLocation(600, 250);
