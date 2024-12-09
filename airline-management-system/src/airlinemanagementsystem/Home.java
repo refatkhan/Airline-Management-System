c94a58c8e34013dbc89a8fc3f77c6a71b26d0cd6
refatkhan <refatkhan897@gmail.com>
2024-12-09 21:13:34 +0600

    add image

--- airline-management-system/src/airlinemanagementsystem/Home.java
+++ airline-management-system/src/airlinemanagementsystem/Home.java
@@ -10,7 +10,13 @@ public class Home extends JFrame implements ActionListener {
     public Home() {
 
         setLayout(null);
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("airlinemanagementsystem/icons/front.jpg"));
+        JLabel image = new JLabel(imageIcon);
+        image.setBounds(0, 0, 1600, 800);
+        add(image);
 
+        // setSize(600, 350);
+        // setLocation(600, 250);
         // size will be max as screensize
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setVisible(true);
