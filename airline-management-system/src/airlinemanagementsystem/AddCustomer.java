755780f8dbd0df101af15d376637caa9bcf05a09
refatkhan <refatkhan897@gmail.com>
2024-12-10 02:42:46 +0600

    add heading and lavel

--- airline-management-system/src/airlinemanagementsystem/AddCustomer.java
+++ airline-management-system/src/airlinemanagementsystem/AddCustomer.java
@@ -7,8 +7,22 @@ public class AddCustomer extends JFrame {
 
     public AddCustomer() {
 
-        getContentPane().setBackground(Color.darkGray);
+        getContentPane().setBackground(Color.gray);
         setLayout(null);
+        // heading
+        JLabel heading = new JLabel("Add Customer Details");
+        heading.setBounds(250, 20, 450, 40);
+        heading.setFont(new Font("Tahoma", Font.BOLD, 30));
+        heading.setForeground(Color.WHITE);
+        add(heading);
+        // lavelname
+        JLabel lblName = new JLabel("Name: ");
+        lblName.setBounds(50, 80, 150, 20);
+        lblName.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lblName);
+
+    //textfield
+    
 
         setSize(900, 600);
         setLocation(300, 150);
