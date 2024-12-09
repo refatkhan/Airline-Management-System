346674cd1353675c28135f91543e98fe50b3751e
refatkhan <refatkhan897@gmail.com>
2024-12-09 21:35:03 +0600

    add menubar for ticket option

--- airline-management-system/src/airlinemanagementsystem/Home.java
+++ airline-management-system/src/airlinemanagementsystem/Home.java
@@ -27,13 +27,31 @@ public class Home extends JFrame implements ActionListener {
 
         JMenuBar menubar = new JMenuBar();
         setJMenuBar(menubar);
-        // add menubar
+        // add details menubar
         JMenu details = new JMenu("Details");
         menubar.add(details);
-        // add menu iteams
+        // add menu iteams for details menu
         JMenuItem flightDetails = new JMenu("Flight Details");
         details.add(flightDetails);
+        JMenuItem customerDetails = new JMenu("Customers Details");
+        details.add(customerDetails);
+        JMenuItem reservationDetails = new JMenu("Reservation Details");
+        details.add(reservationDetails);
+        JMenuItem journeyDetails = new JMenu("Journey Details");
+        details.add(journeyDetails);
+        JMenuItem bookDetails = new JMenu("Book Ticket");
+        details.add(bookDetails);
+        JMenuItem cancelDetails = new JMenu("Cancel Ticket");
+        details.add(cancelDetails);
 
+        // menubar for ticket
+        JMenu ticket = new JMenu("Ticket");
+        menubar.add(ticket);
+        // add menu iteams
+        JMenuItem ticketDetails = new JMenu("Ticket Details");
+        ticket.add(ticketDetails);
+        JMenuItem bordingDetails = new JMenu("Bording Details");
+        ticket.add(bordingDetails);
         // setSize(600, 350);
         // setLocation(600, 250);
         // size will be max as screensize
