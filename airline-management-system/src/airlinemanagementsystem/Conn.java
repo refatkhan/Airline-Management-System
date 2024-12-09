af71c636cca3e86c8bcff4060d97b63743b92140
refatkhan <refatkhan897@gmail.com>
2024-12-09 19:04:35 +0600

    start again

--- airline-management-system/src/airlinemanagementsystem/Conn.java
+++ airline-management-system/src/airlinemanagementsystem/Conn.java
@@ -12,14 +12,14 @@ import java.sql.*;
  * @author REFAT KHAN
  */
 public class Conn {
-    
+
     Connection c;
     Statement s;
-    
+
     public Conn() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem", "root", "codeforinterview");
+            c = DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem", "root", "root");
             s = c.createStatement();
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
--- airline-management-system/src/airlinemanagementsystem/Login.java
+++ airline-management-system/src/airlinemanagementsystem/Login.java
@@ -7,48 +7,48 @@ public class Login extends JFrame {
     JButton submit, reset, close;
     JTextField tfusername;
     JPasswordField tfpassword;
-    
+
     public Login() {
         getContentPane().setBackground(Color.yellow);
         setLayout(null);
-        
+
         JLabel lblusername = new JLabel("Username");
         lblusername.setBounds(20, 20, 100, 20);
         add(lblusername);
-        
+
         tfusername = new JTextField();
         tfusername.setBounds(130, 20, 200, 20);
         add(tfusername);
-        
+
         JLabel lblpassword = new JLabel("Password");
         lblpassword.setBounds(20, 60, 100, 20);
         add(lblpassword);
-        
+
         tfpassword = new JPasswordField();
         tfpassword.setBounds(130, 60, 200, 20);
         add(tfpassword);
-        
-        reset = new JButton("Reset");
+
+        reset = new JButton("Forget Password");
         reset.setBounds(40, 120, 120, 20);
-        
+
         add(reset);
-        
+
         submit = new JButton("Submit");
         submit.setBounds(190, 120, 120, 20);
-       
+
         add(submit);
-        
+
         close = new JButton("Close");
         close.setBounds(120, 160, 120, 20);
-       
+
         add(close);
-        
-        setSize(400, 250);
+
+        setSize(600, 350);
         setLocation(600, 250);
         setVisible(true);
     }
-    
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
         new Login();
     }
 
