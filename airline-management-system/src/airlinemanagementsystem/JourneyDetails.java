60b391c8c4c48ee65b981405895f3cb221795ae9
refatkhan <refatkhan897@gmail.com>
2024-12-11 23:55:04 +0600

    fixed the all error

--- airline-management-system/src/airlinemanagementsystem/JourneyDetails.java
+++ airline-management-system/src/airlinemanagementsystem/JourneyDetails.java
@@ -7,7 +7,6 @@ import java.awt.event.*;
 import net.proteanit.sql.DbUtils;
 
 public class JourneyDetails extends JFrame implements ActionListener {
-
     JTable table;
     JTextField pnr;
     JButton show;
@@ -22,6 +21,10 @@ public class JourneyDetails extends JFrame implements ActionListener {
         lblpnr.setBounds(50, 50, 100, 25);
         add(lblpnr);
 
+        pnr = new JTextField();
+        pnr.setBounds(160, 50, 120, 25);
+        add(pnr);
+
         show = new JButton("Show Details");
         show.setBackground(Color.BLACK);
         show.setForeground(Color.WHITE);
@@ -33,7 +36,7 @@ public class JourneyDetails extends JFrame implements ActionListener {
 
         JScrollPane jsp = new JScrollPane(table);
         jsp.setBounds(0, 100, 800, 150);
-        jsp.setBackground(Color.white);
+        jsp.setBackground(Color.WHITE);
         add(jsp);
 
         setSize(800, 600);
@@ -43,14 +46,17 @@ public class JourneyDetails extends JFrame implements ActionListener {
 
     public void actionPerformed(ActionEvent ae) {
         try {
-            Connect connect = new Connect();
+            Connect conn = new Connect();
+            ResultSet rs = conn.statement.executeQuery("select * from reservation where PNR = '" + pnr.getText() + "'");
 
-            ResultSet rs = conn.s.executeQuery("select * from reservation where PNR = '" + pnr.getText() + "'");
+            if (!rs.isBeforeFirst()) {
+                JOptionPane.showMessageDialog(null, "No Information Found");
+                return;
+            }
             table.setModel(DbUtils.resultSetToTableModel(rs));
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
     public static void main(String[] args) {
