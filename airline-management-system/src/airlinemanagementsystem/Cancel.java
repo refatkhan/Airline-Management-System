06ad8582f74d81c9af670c405edb6f09056dd7c3
refatkhan <refatkhan897@gmail.com>
2024-12-12 00:18:50 +0600

    add PNR number option and a text field

--- airline-management-system/src/airlinemanagementsystem/Cancel.java
+++ airline-management-system/src/airlinemanagementsystem/Cancel.java
@@ -8,8 +8,9 @@ import java.util.*;
 import java.util.Random;
 
 public class Cancel extends JFrame implements ActionListener {
-
     JTextField tfpnr;
+    JLabel tfname, cancellationno, lblfcode, lbldateoftravel;
+    JButton fetchButton, flight;
 
     public Cancel() {
 
@@ -22,6 +23,14 @@ public class Cancel extends JFrame implements ActionListener {
         heading.setBounds(180, 20, 250, 35);
         heading.setFont(new Font("Tahoma", Font.PLAIN, 32));
         add(heading);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("airlinemanagementsystem/icons/cancel.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(250, 250, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(470, 120, 250, 250);
+        add(image);
+
         JLabel lblnid = new JLabel("PNR Number");
         lblnid.setBounds(60, 80, 150, 25);
         lblnid.setFont(new Font("Tahoma", Font.PLAIN, 16));
@@ -30,12 +39,7 @@ public class Cancel extends JFrame implements ActionListener {
         tfpnr.setBounds(220, 80, 150, 25);
         add(tfpnr);
 
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("airlinemanagementsystem/icons/cancel.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(250, 250, Image.SCALE_DEFAULT);
-        ImageIcon i3 = new ImageIcon(i2);
-        JLabel image = new JLabel(i3);
-        image.setBounds(470, 120, 250, 250);
-        add(image);
+        
 
         setSize(800, 450);
         setLocation(350, 150);
