4bab4dbd8728d6ba621ef4b2e0e9070eccd38bb5
refatkhan <refatkhan897@gmail.com>
2024-12-12 03:02:55 +0600

    add image

--- airline-management-system/src/airlinemanagementsystem/Cancel.java
+++ airline-management-system/src/airlinemanagementsystem/Cancel.java
@@ -73,13 +73,15 @@ public class Cancel extends JFrame implements ActionListener {
         lblsrc.setBounds(220, 220, 150, 25);
         add(lblsrc);
 
-        JLabel lblgender = new JLabel("Date");
-        lblgender.setBounds(60, 280, 150, 25);
+        JLabel lblgender = new JLabel("DEST");
+        lblgender.setBounds(380, 220, 150, 25);
         lblgender.setFont(new Font("Tahoma", Font.PLAIN, 16));
         add(lblgender);
+
         lbldest = new JLabel();
         lbldest.setBounds(540, 220, 150, 25);
         add(lbldest);
+
         JLabel lblfname = new JLabel("Flight Name");
         lblfname.setBounds(60, 260, 150, 25);
         lblfname.setFont(new Font("Tahoma", Font.PLAIN, 16));
@@ -88,6 +90,32 @@ public class Cancel extends JFrame implements ActionListener {
         labelfname = new JLabel();
         labelfname.setBounds(220, 260, 150, 25);
         add(labelfname);
+
+        JLabel lblfcode = new JLabel("Flight Code");
+        lblfcode.setBounds(380, 260, 150, 25);
+        lblfcode.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lblfcode);
+
+        labelfcode = new JLabel();
+        labelfcode.setBounds(540, 260, 150, 25);
+        add(labelfcode);
+
+        JLabel lbldate = new JLabel("Date");
+        lbldate.setBounds(60, 300, 150, 25);
+        lbldate.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lbldate);
+
+        labeldate = new JLabel();
+        labeldate.setBounds(220, 300, 150, 25);
+        add(labeldate);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("airlinemanagementsystem/icons/airindia.png"));
+        Image i2 = i1.getImage().getScaledInstance(300, 230, Image.SCALE_DEFAULT);
+        ImageIcon image = new ImageIcon(i2);
+        JLabel lblimage = new JLabel(image);
+        lblimage.setBounds(600, 0, 300, 300);
+        add(lblimage);
+
         setSize(800, 450);
         setLocation(350, 150);
         setVisible(true);
