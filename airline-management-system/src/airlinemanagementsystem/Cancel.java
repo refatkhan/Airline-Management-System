7ceabdf77655d1768e8f098c64cb1e8ced4a4cbf
refatkhan <refatkhan897@gmail.com>
2024-12-12 03:07:36 +0600

    updated

--- airline-management-system/src/airlinemanagementsystem/Cancel.java
+++ airline-management-system/src/airlinemanagementsystem/Cancel.java
@@ -10,8 +10,8 @@ import java.util.Random;
 public class Cancel extends JFrame implements ActionListener {
 
     JTextField tfpnr;
-    JLabel tfname, tfnationality, lblsrc, lbldest, labelfname, labelfcode, labeldate;
-    JButton fetchButton;
+    JLabel tfname, cancellationno, lblfcode, lbldateoftravel;
+    JButton fetchButton, flight;
 
     public Cancel() {
 
@@ -60,61 +60,35 @@ public class Cancel extends JFrame implements ActionListener {
         lblnationality.setBounds(60, 180, 150, 25);
         lblnationality.setFont(new Font("Tahoma", Font.PLAIN, 16));
         add(lblnationality);
-        tfnationality = new JLabel();
-        tfnationality.setBounds(220, 180, 150, 25);
-        add(tfnationality);
 
-        JLabel lbladdress = new JLabel("SRC");
-        lbladdress.setBounds(60, 220, 150, 25);
+        cancellationno = new JLabel("" + random.nextInt(1000000));
+        cancellationno.setBounds(220, 180, 150, 25);
+        add(cancellationno);
+
+        JLabel lbladdress = new JLabel("Flight Code");
+        lbladdress.setBounds(60, 230, 150, 25);
         lbladdress.setFont(new Font("Tahoma", Font.PLAIN, 16));
         add(lbladdress);
 
-        lblsrc = new JLabel();
-        lblsrc.setBounds(220, 220, 150, 25);
-        add(lblsrc);
+        lblfcode = new JLabel();
+        lblfcode.setBounds(220, 230, 150, 25);
+        add(lblfcode);
 
-        JLabel lblgender = new JLabel("DEST");
-        lblgender.setBounds(380, 220, 150, 25);
+        JLabel lblgender = new JLabel("Date");
+        lblgender.setBounds(60, 280, 150, 25);
         lblgender.setFont(new Font("Tahoma", Font.PLAIN, 16));
         add(lblgender);
 
-        lbldest = new JLabel();
-        lbldest.setBounds(540, 220, 150, 25);
-        add(lbldest);
+        lbldateoftravel = new JLabel();
+        lbldateoftravel.setBounds(220, 280, 150, 25);
+        add(lbldateoftravel);
 
-        JLabel lblfname = new JLabel("Flight Name");
-        lblfname.setBounds(60, 260, 150, 25);
-        lblfname.setFont(new Font("Tahoma", Font.PLAIN, 16));
-        add(lblfname);
-
-        labelfname = new JLabel();
-        labelfname.setBounds(220, 260, 150, 25);
-        add(labelfname);
-
-        JLabel lblfcode = new JLabel("Flight Code");
-        lblfcode.setBounds(380, 260, 150, 25);
-        lblfcode.setFont(new Font("Tahoma", Font.PLAIN, 16));
-        add(lblfcode);
-
-        labelfcode = new JLabel();
-        labelfcode.setBounds(540, 260, 150, 25);
-        add(labelfcode);
-
-        JLabel lbldate = new JLabel("Date");
-        lbldate.setBounds(60, 300, 150, 25);
-        lbldate.setFont(new Font("Tahoma", Font.PLAIN, 16));
-        add(lbldate);
-
-        labeldate = new JLabel();
-        labeldate.setBounds(220, 300, 150, 25);
-        add(labeldate);
-
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("airlinemanagementsystem/icons/airindia.png"));
-        Image i2 = i1.getImage().getScaledInstance(300, 230, Image.SCALE_DEFAULT);
-        ImageIcon image = new ImageIcon(i2);
-        JLabel lblimage = new JLabel(image);
-        lblimage.setBounds(600, 0, 300, 300);
-        add(lblimage);
+        flight = new JButton("Cancel");
+        flight.setBackground(Color.BLACK);
+        flight.setForeground(Color.WHITE);
+        flight.setBounds(220, 330, 120, 25);
+        flight.addActionListener(this);
+        add(flight);
 
         setSize(800, 450);
         setLocation(350, 150);
